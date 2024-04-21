0f40ce0b648bab80b3e212104ee399d24c99b26d
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2024-04-21 17:26:07 -0500

    :rocket: (SET04-CAP25) Entities and DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add a global prefix to all routes
+  app.setGlobalPrefix('api/v1');
+
+  // Add a global validation pipe, which will validate all incoming requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -17,7 +17,7 @@ export class ProductsController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+    return createProductDto;
   }
 
   @Get()
